8b5c75c9efea03a7c251a7729a4a74bab9f67d45
RobinD95 <lopfi95@gmail.com>
2020-11-24 07:47:07 +0100

    converted to electron app

--- main.js
+++ main.js
@@ -2,23 +2,29 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path')
 
-const {app, BrowserWindow, Menu} = electron;
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 
 let mainWindow, addWindow;
 
 // Listen for app to be ready
 app.on('ready', function () {
    //Create new Window
-   mainWindow = new BrowserWindow({});
+   mainWindow = new BrowserWindow({
+       webPreferences: {
+           nodeIntegration: true
+       }
+   });
    //Load html into window
-    mainWindow.loadFile('index.html')
-
+    mainWindow.loadFile('index.html');
+    //quit app when closed
+    mainWindow.on('closed', function () {
+        app.quit();
+    });
     //Build Menu from template
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
     //Insert menu
     Menu.setApplicationMenu(mainMenu);
 });
-
 //Handle create add window
 function createAddWindow() {
     //Create new Window
@@ -28,8 +34,19 @@ function createAddWindow() {
         title: 'füge einen neuen Termin ein'
     });
     //Load html into window
-    addWindow.loadFile('add.html')
+    addWindow.loadFile('addWindow.html');
+    //Garbage collection handle
+    addWindow.on('close', function () {
+        addWindow = null;
+    });
 }
+
+//Catch subject:add
+ipcMain.on('item:add', function (e, {name, link}) {
+    console.log({name, link});
+    mainWindow.webContents.send('subject:add', {name, link});
+    addWindow.close();
+})
 
 //Create Menu Template
 const mainMenuTemplate = [
@@ -37,10 +54,10 @@ const mainMenuTemplate = [
         label: 'File',
         submenu: [
             {
-                label: 'Load table'
+                label: 'Vorlage importieren'
             },
             {
-                label: 'add something',
+                label: 'Fach hinzufügen',
                 accelerator: process.platform == 'darwin' ? 'Command+A' : 'Ctrl+A',
                 click(){
                     createAddWindow();
@@ -49,3 +66,27 @@ const mainMenuTemplate = [
         ]
     }
 ]
+
+//If mac, add empty object to menu
+if (process.platform == "darwin") {
+    mainMenuTemplate.unshift({})
+}
+
+//Add dev tools if not in prod
+if (process.env.NODE_ENV !== 'production') {
+    mainMenuTemplate.push({
+        label: 'DevTools',
+        submenu: [
+            {
+                label: 'toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    });
+}
